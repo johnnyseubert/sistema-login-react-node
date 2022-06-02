ebf22475ee4d7b63fe62b489a43c897562563def
Johnny <johnny.seubert@pulsati.com.br>
2022-06-02 10:22:56 -0300

    2º modificando header react

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -6,30 +6,20 @@ import './style.css'
 export default function Header() {
 
     const { logged, userName } = useContext(UserLoginContext)
-    const [mostrarMenus, setMostrarMenus] = useState(false);
-
-    useEffect(() => {
-        if (logged == true) {
-            setMostrarMenus(true)
-        } else {
-            setMostrarMenus(false)
-        }
-    }, [logged])
 
     return (
         <header className='topo'>
             <div className='container'>
-                {logged && <h3><span className='dourado'>Seja bem vindo {userName}!</span></h3>}
-
-                <nav>
-                    {mostrarMenus && (
-                        <>
+                {logged && (
+                    <>
+                        <h3><span className='dourado'>Seja bem vindo {userName}!</span></h3>
+                        <nav>
                             <Link to="/home">Home</Link >
                             <Link to="/sobre">Sobre</Link >
                             <Link to="/">Desconectar</Link >
-                        </>
-                    )}
-                </nav>
+                        </nav>
+                    </>
+                )}
             </div>
         </header>
     )
